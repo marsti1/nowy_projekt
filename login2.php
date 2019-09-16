05c6c86b0e0b2f57069ed046e84f21b09f2157a8
marsti1 <tableciak.tablet@gmail.com>
2019-09-16 09:21:38 +0200

    poprawki logowanie

--- login2.php
+++ login2.php
@@ -26,26 +26,28 @@
 	}
 ?>
 
-	<div class="cont">
-        <div id="modal-content">
-        	<div class="test">&times</div>
-        	<h1>Zaloguj się</h1>
-        	<form action="zaloguj.php" method="POST">
-        		<label for="username" class="col-sm-3">Login lub e-mail:</label>
-        		<input type="text" name="username" required />
-        		<br/>
-        		<label for="password" class="col-sm-3">Hasło:</label>
-        		<input type="password" name="password" required />
-        		<br/>
-        		<button type="submit" class="btn btn-primary">Zaloguj się</button>
-        	</form>
-        	<p><?php echo $komunikat; ?></p>
-        	<div class="login" style="background-color:#f1f1f1">
-			    <button type="button" class="cancelbtn">Cancel</button>
-			    <span class="psw">Forgot <a href="#">password?</a></span>
-			</div>
-        </div>
- 
+	
+		
+	        <div class="modal-content">
+	        	<div class="cont">
+	        	<div class="test">&times</div>
+	        	<h1>Zaloguj się</h1>
+	        	<form action="zaloguj.php" method="POST">
+	        		<label for="username" class="col-sm-3">Login lub e-mail:</label>
+	        		<input type="text" name="username" required />
+	        		<br/>
+	        		<label for="password" class="col-sm-3">Hasło:</label>
+	        		<input type="password" name="password" required />
+	        		<br/>
+	        		<button type="submit" class="btn btn-primary">Zaloguj się</button>
+	        	</form>
+	        	<p><?php echo $komunikat; ?></p>
+	        	<div class="login" style="background-color:#f1f1f1">
+				    <button type="button" class="cancelbtn">Cancel</button>
+				    <span class="psw">Forgot <a href="#">password?</a></span>
+				</div>
+	        </div>
+ 		
 	</div>
 	
 	
